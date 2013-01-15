8a403b6bfdbe6409b25896bcef4bbff368a5bbc4
Hugo Cordeiro <hugo.ocordeiro@gmail.com>
2013-01-15 00:35:47 -0300

    OpticalFlow update V
    Still have to improve

--- js/blocks_vision.js
+++ js/blocks_vision.js
@@ -83,14 +83,16 @@ var opticalFlow = function () {
 	var initData = VB.interpreter.dictionary["initData"];
 	VB.interpreter.dictionary["initData"] = data;
 
-	var winSize = 4;
+	var winSize = 10;
 	var winStep = winSize * 2 + 1;
 
 	var i,j,k,l,address;
 
 	var gradX, gradY, gradT;
 	var A2, A1B2, B1, C1, C2;
-	var u, v;
+	var u, v, uu, vv, n;
+
+	uu = vv = n = 0;
 	
 	var width = VB.interpreter.dictionary["workingArea"].width;
 	var height = VB.interpreter.dictionary["workingArea"].height;
@@ -98,6 +100,7 @@ var opticalFlow = function () {
 	var wmax = (width*4) - (winSize*4) - 4;
 	var hmax = height - winSize - 1;
 
+	var pVX, nVX, pVY, nVY, pVT, nVT;
 	// http://www.mathworks.com/help/vision/ref/vision.opticalflowclass.html
 	for (i = winSize + 1; i < hmax; i+=winStep) {
 		for (j = (winSize*4); j < wmax; j += (winStep*4)) {
@@ -107,10 +110,19 @@ var opticalFlow = function () {
 				for (l = -(winSize*4); l <= winSize*4; l+=4) {
 					
 					address = (i + k) * (width*4) + j + l;
-					
-					gradX = (data[address - 4] & 0xff) - (data[address + 4] & 0xff);
-					gradY = (data[address - (width*4)] & 0xff) - (data[address + (width*4)] & 0xff);
-					gradT = (initData[address] & 0xff) - (data[address] & 0xff);
+
+					pVX = Math.max(data[address-4],data[address-3],data[address-2]);
+					nVX = Math.max(data[address+4],data[address+5],data[address+6]);
+
+					pVY = Math.max(data[address-(width*4)],data[address-(width*4)+1],data[address-(width*4)+2]);
+					nVY = Math.max(data[address+(width*4)],data[address+(width*4)+1],data[address+(width*4)+2]);
+
+					pVT = Math.max(initData[address], initData[address+1], initData[address+2]);
+					nVT = Math.max(data[address], data[address+1], data[address+2]);
+
+					gradX = (pVX & 0xff) - (nVX & 0xff);
+					gradY = (pVY & 0xff) - (nVY & 0xff);
+					gradT = (pVT & 0xff) - (nVT & 0xff);
 
 					A2 += gradX * gradX;
 					A1B2 += gradX * gradY;
@@ -122,28 +134,28 @@ var opticalFlow = function () {
 			var delta = (A1B2 * A1B2 - A2 * B1);
 
 			if (delta) {
-					/* system is not singular - solving by Kramer method */
-					var deltaX, deltaY;
-					var Idelta = 8 / delta;
+				/* system is not singular - solving by Kramer method */
+				var deltaX, deltaY;
+				var Idelta = 8 / delta;
 
-					deltaX = -(C1 * A1B2 - C2 * B1);
-					deltaY = -(A1B2 * C2 - A2 * C1);
+				deltaX = -(C1 * A1B2 - C2 * B1);
+				deltaY = -(A1B2 * C2 - A2 * C1);
 
-					u = deltaX * Idelta;
-					v = deltaY * Idelta;
+				u = deltaX * Idelta;
+				v = deltaY * Idelta;
+			} else {
+				/* singular system - find optical flow in gradient direction */
+				var Norm = (A1B2 + A2) * (A1B2 + A2) + (B1 + A1B2) * (B1 + A1B2);
+
+				if (Norm) {
+					var IGradNorm = 8 / Norm;
+					var temp = -(C1 + C2) * IGradNorm;
+					u = (A1B2 + A2) * temp;
+					v = (B1 + A1B2) * temp;
 				} else {
-					/* singular system - find optical flow in gradient direction */
-					var Norm = (A1B2 + A2) * (A1B2 + A2) + (B1 + A1B2) * (B1 + A1B2);
-
-					if (Norm) {
-						var IGradNorm = 8 / Norm;
-						var temp = -(C1 + C2) * IGradNorm;
-						u = (A1B2 + A2) * temp;
-						v = (B1 + A1B2) * temp;
-					} else {
-						u = v = 0;
-					}
+					u = v = 0;
 				}
+			}
 
 			var scaleX = scaleY = Math.sqrt(u * u + v * v);
 
@@ -157,19 +169,29 @@ var opticalFlow = function () {
 
 			var toDegree = 180 / Math.PI;
 			var rotation = Math.atan2(v, u);
-			// var rgb = 'rgb('+((v/scaleX)*128+128)+','+((u/scaleY)*128+128)+',0)';
-			// ctx.strokeStyle = rgb;
-
 			ctx.beginPath();
-
-			// var y = i-winSize;
-			// var x = (j/4)-winSize;
-			// ctx.rect( x, y, winSize*2, winSize*2 );
-
 			ctx.moveTo((j-1)/4 , i);
 			ctx.lineTo(((j-1)/4)+scaleX*Math.cos(rotation), i+scaleY*Math.sin(rotation));
-			
+			ctx.strokeStyle = "#000000"
+			ctx.lineWidth = 1;
 			ctx.stroke();
+			if (-winStep < u && u < winStep && -winStep < v && v < winStep) {
+				uu += u;
+				vv += v;
+				n++;
+			}
 		}
 	}
+	uu /= n;
+	vv /= n;
+	var scale = Math.sqrt(uu*uu+vv*vv)*25; 
+	var toDegree = 180 / Math.PI;
+	var rotation = Math.atan2(vv, uu);
+	ctx.beginPath();
+	ctx.moveTo(width/2, height/2);
+	ctx.lineTo((width/2)+scale*Math.cos(rotation), (height/2)+scale*Math.sin(rotation));
+	ctx.lineWidth = 5;
+	ctx.strokeStyle = "#ff0000";
+	ctx.stroke();
+
 }
