eeff69496382a5c859c8a0a67aa1eac1008ac480
Andrei Ivanov <aivanov@mit.edu>
2013-04-24 00:49:55 -0400

    Motion detection screenshots (storecam, foodcam)

---
+++ js/load_link.js
@@ -0,0 +1,61 @@
+      <div id="block-play-forever" class="build-block block-container-start build-block-1" data-block-name="playForever" style="">
+              <div class="build-block-title-wrapper build-block-recalc-width" style="width: 194px;">
+                <div class="build-block-title">
+                  <div class="icon1-build-block icon1-play-forever"></div>
+                  <span class="play-title">Play Forever</span>
+                  <div class="icon-remove-block"></div>
+                </div>
+              </div>
+              <div class="build-block-top"></div>
+              <div class="content" style="height: 166px;">
+        <div class="build-block-1-leftbar" style="height: 187px;"></div>
+	
+
+					<div id="block-storecam" class="build-block block-container-source build-block-2" data-block-name="loadStorecamA">
+                    <div class="build-block-2-top build-block-recalc-width"></div>
+                    <div class="build-block-2-title-wrapper build-block-recalc-width">
+                      <div class="build-block-2-title">
+                        <div class="icon1-build-block icon1-webcam"></div>
+                        <span>StorecamA</span>
+                        <div class="icon-remove-block"></div>
+                      </div>
+                    </div>
+                    <div class="build-block-2-bottom build-block-recalc-width"></div>
+                  </div>
+
+				<div id="block-storecam" class="build-block block-container-source build-block-2" data-block-name="loadStorecamB" style="top: -20px; left: 12px;">
+                  <div class="build-block-2-top build-block-recalc-width" style="width: 182px;"></div>
+                  <div class="build-block-2-title-wrapper build-block-recalc-width" style="width: 182px;">
+                    <div class="build-block-2-title">
+                      <div class="icon1-build-block icon1-webcam"></div>
+                      <span>StorecamB</span>
+                      <div class="icon-remove-block"></div>
+                    </div>
+                  </div>
+                  <div class="build-block-2-bottom build-block-recalc-width" style="width: 182px;"></div>
+                  </div>
+
+
+	<div id="block-play-forever" class="build-block block-container-start build-block-1" data-block-name="playForever" >
+                    <div class="build-block-title-wrapper build-block-recalc-width">
+                      <div class="build-block-title">
+                        <div class="icon1-build-block icon1-play-forever"></div>
+                        <span class="play-title">Play Forever</span>
+                        <div class="icon-remove-block"></div>
+                      </div>
+                    </div>
+                    <div class="build-block-top"></div>
+                    <div class="content">
+                      <div class="build-block-1-leftbar"></div>
+                      <div class='build-block empty-block'></div>
+                    </div>
+                    <div class="build-block-bottom build-block-recalc-width"></div>
+                    <div class="build-block-1-bottom-fill build-block-recalc-width"></div>
+                  </div>
+
+
+if (window.location.search!=''){
+	var urlParam=$.url().param()
+	var blocks=urlParam.blocks
+
+} 
